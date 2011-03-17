b10b45251ee2f00f652fec66fe557aef31a92189
rharagutchi@gmail.com <rharagutchi@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-03-17 17:24:07 +0000

    Initial import of BackEnd client. Created a FrontEndUpdate class

---
+++ BackendClient/src/FrontEndUpdate.java
@@ -0,0 +1,108 @@
+
+public class FrontEndUpdate {
+	public static enum UpdateType {
+		Notification,
+		Markup
+	}
+	public static enum MarkupType {
+		Insert, 
+		Delete,
+		Cursor,
+		Highlight
+	}
+	public static enum NotificationType {
+		New_Connection,
+		Connection_Error,
+		Connection_Succeed,
+		New_Shared_File,
+		Close_Shared_File,
+		Get_Shared_File,
+		User_Connected,
+		User_Disconnected,
+		Request_Host,
+		New_Host,
+		Host_Disconnect,
+		Server_Disconnect
+	}
+	private int startLocation, endLocation;
+	private int fileId, userId;
+	private String username, url, errorMessage, insert;
+	private UpdateType updateType;
+	private MarkupType markupType;
+	private NotificationType notificationType;
+	
+	public FrontEndUpdate(UpdateType updateType) {
+		this.updateType = updateType;
+	}
+	
+	// Getters and Setters
+	public int getStartLocation() {
+		return startLocation;
+	}
+	public void setStartLocation(int startLocation) {
+		this.startLocation = startLocation;
+	}
+	public int getEndLocation() {
+		return endLocation;
+	}
+	public void setEndLocation(int endLocation) {
+		this.endLocation = endLocation;
+	}
+	public int getFileId() {
+		return fileId;
+	}
+	public void setFileId(int fileId) {
+		this.fileId = fileId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	public String getInsert() {
+		return insert;
+	}
+	public void setInsert(String insert) {
+		this.insert = insert;
+	}
+
+	public void setMarkupType(MarkupType markupType) {
+		this.markupType = markupType;
+	}
+
+	public MarkupType getMarkupType() {
+		return markupType;
+	}
+
+	public void setNotificationType(NotificationType notificationType) {
+		this.notificationType = notificationType;
+	}
+
+	public NotificationType getNotificationType() {
+		return notificationType;
+	}
+
+	// Purposely not having a setUpdateType
+	public UpdateType getUpdateType() {
+		return updateType;
+	}
+}
