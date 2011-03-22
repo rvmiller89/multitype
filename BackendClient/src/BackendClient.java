5f97798e5773f2bbabb07ab97fa2b9d7ec4c9ec1
rharagutchi@gmail.com <rharagutchi@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-03-22 00:42:25 +0000

    finished writing first draft of this file

--- BackendClient/src/BackendClient.java
+++ BackendClient/src/BackendClient.java
@@ -1,3 +1,11 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * The BackendClient is used as means of communication with the 
  * BackendServer. 
@@ -5,14 +13,78 @@
  *
  */
 public class BackendClient {
+	private Socket serverSocket;
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
+	private Thread receiveUpdateThread;
+	private Thread sendUpdateThread;
+	private BlockingQueue<FrontEndUpdate> fromServerQueue;
+	private BlockingQueue<FrontEndUpdate> fromFrontEndQueue;
+	private boolean done = false;
 	
+	/**
+	 * Constructor for BackendClient
+	 * @param url Url to connect to, can be domain name or ip
+	 * @param port port to be used to connect to
+	 */
+	public BackendClient(String url, int port) {
+		fromServerQueue = new LinkedBlockingQueue<FrontEndUpdate>();
+		fromFrontEndQueue = new LinkedBlockingQueue<FrontEndUpdate>();
+		try {
+			serverSocket = new Socket(url, port);
+            out = new ObjectOutputStream(serverSocket.getOutputStream());
+            in = new ObjectInputStream(serverSocket.getInputStream());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		receiveUpdateThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(!done) {
+					try {
+						FrontEndUpdate feu = 
+							(FrontEndUpdate)in.readObject();
+						fromServerQueue.add(feu);
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+					
+				}
+			}			
+		});
+		receiveUpdateThread.start();
+		
+		sendUpdateThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(!done) {
+					try {
+						FrontEndUpdate feu = fromFrontEndQueue.take();
+						out.writeObject(feu);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}			
+		});
+		sendUpdateThread.start();
+		
+	}
 	
 	/**
 	 * Sends a FrontEndUpdate to the server
 	 * @param feu Pre-constructed FrontEndUpdate to be sent
 	 */
 	public void sendUpdate(FrontEndUpdate feu) {
-		
+		fromFrontEndQueue.add(feu);
+		System.out.println(feu.getInsert());
 	}
 	
 	/**
@@ -20,14 +92,19 @@ public class BackendClient {
 	 * @return next FEU from Queue
 	 */
 	public FrontEndUpdate getUpdate() {
+		try {
+			return fromServerQueue.take();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * needs to be called in between sessions. We spun off thread, we need to 
+	 * kill them
 	 */
-	public boolean hasUpdate() {
-		return false;
+	public void finish() {
+		done = true;
 	}
 }
