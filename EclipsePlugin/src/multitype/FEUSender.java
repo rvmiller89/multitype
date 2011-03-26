7289ff12de3d7610e8fd5ebbb6f58e83f6aa4600
rvmiller89@gmail.com <rvmiller89@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-03-26 21:46:21 +0000

    Added FEUSender to send FEUs

---
+++ EclipsePlugin/src/multitype/FEUSender.java
@@ -0,0 +1,15 @@
+package multitype;
+
+public class FEUSender {
+
+	private FEUSender() {
+		
+	}
+	
+	public static void send(FrontEndUpdate feu)
+	{
+		BackendClient bc = Activator.getDefault().client;
+		bc.sendUpdate(feu);
+	}
+
+}
