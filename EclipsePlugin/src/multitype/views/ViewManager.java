cc12c4dce17d425d394f9360c92bcdc24f6931d0
azfarkhandoker@gmail.com <azfarkhandoker@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-03-26 00:11:15 +0000

    added receive() stub for ViewManager

--- EclipsePlugin/src/multitype/views/ViewManager.java
+++ EclipsePlugin/src/multitype/views/ViewManager.java
@@ -1,9 +1,16 @@
 package multitype.views;
 
+import multitype.FrontEndUpdate;
+
 public class ViewManager {
 
 	public ViewManager() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void receive(FrontEndUpdate feu)
+	{
+		// TODO
 	}
 
 }
