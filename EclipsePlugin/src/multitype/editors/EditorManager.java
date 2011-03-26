31625090646c02b2638e64883ef5f2ba3def8b94
azfarkhandoker@gmail.com <azfarkhandoker@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-03-26 02:17:08 +0000

    finished insert(), delete(), receive() for build1

--- EclipsePlugin/src/multitype/editors/EditorManager.java
+++ EclipsePlugin/src/multitype/editors/EditorManager.java
@@ -1,16 +1,80 @@
 package multitype.editors;
 
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
+
 import multitype.FrontEndUpdate;
+import multitype.FrontEndUpdate.MarkupType;
 
-public class EditorManager {
-
-	public EditorManager() {
-		
+public class EditorManager
+{
+	private ITextEditor editor;
+	private IDocumentProvider dp;
+	private IDocument doc;
+	
+	public EditorManager()
+	{
+		editor = ActiveEditor.getEditor();
+	    dp = editor.getDocumentProvider();
+	    doc = dp.getDocument(editor.getEditorInput());
 	}
 	
 	public void receive(FrontEndUpdate feu)
 	{
+		MarkupType markupType = feu.getMarkupType();
 		
+		switch(markupType)
+		{
+		case Cursor:
+			cursorPos(feu.getStartLocation(), feu.getUserId());
+			break;
+		case Delete:
+			delete(feu.getFileId(), feu.getUserId(), feu.getStartLocation(), feu.getEndLocation());
+			break;
+		case Highlight:
+			highlight(feu.getFileId(), feu.getUserId(), feu.getStartLocation(), feu.getEndLocation());
+			break;
+		case Insert:
+			insert(feu.getFileId(), feu.getUserId(), feu.getStartLocation(), feu.getInsertString());
+			break;
+		default:
+			throw new IllegalArgumentException("BAD FEU MARKUP TYPE: " + markupType);
+		}
 	}
-
+	
+	private void delete(int fileId, int userId, int fromPos, int toPos)
+	{
+		try {
+			doc.replace(fromPos, toPos - fromPos, "");
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void insert(int fileId, int userId, int fromPos, String string)
+	{
+	    try {
+			doc.replace(fromPos, 0, string);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void highlight(int fileId, int userId, int fromPos, int toPos)
+	{
+		// TODO: build 2
+//		ISelectionProvider selectionProvider = editor.getSelectionProvider();
+//	    ITextSelection selection = (ITextSelection) selectionProvider.getSelection();
+//	    
+//	    String text = selection.getText();
+	}
+	
+	private void cursorPos(int pos, int userId)
+	{
+		// TODO: build 2
+	}
 }
