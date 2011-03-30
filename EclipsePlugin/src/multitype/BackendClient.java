3345da3ce290e8184a2ec1aebdd1411204fb2e76
rharagutchi@gmail.com <rharagutchi@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-03-30 00:13:57 +0000

    same as prev commit

--- EclipsePlugin/src/multitype/BackendClient.java
+++ EclipsePlugin/src/multitype/BackendClient.java
@@ -180,6 +180,9 @@ public class BackendClient {
 			FrontEndUpdate given) {
 		if(toUpdate == given) //don't update itself
 			return;
+		// don't update if the user is the same
+		if(toUpdate.getUserId() == given.getUserId()) 
+			return;
 		if(given.getMarkupType() == FrontEndUpdate.MarkupType.Insert) {
 			int insertAt = given.getStartLocation();
 			int sizeOfInsert = given.getInsertString().length();
