990288ab17c02ed5212fc61a824784115771da4b
rharagutchi@gmail.com <rharagutchi@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-03-30 00:13:07 +0000

    added a critical check in updateFEUgivenFEU

--- Server/MarkupProcessor.java
+++ Server/MarkupProcessor.java
@@ -120,6 +120,9 @@ public class MarkupProcessor implements Runnable{
 			FrontEndUpdate given) {
 		if(toUpdate == given) //don't update itself
 			return;
+		// don't update if the user is the same
+		if(toUpdate.getUserId() == given.getUserId()) 
+			return;
 		if(given.getMarkupType() == FrontEndUpdate.MarkupType.Insert) {
 			int insertAt = given.getStartLocation();
 			int sizeOfInsert = given.getInsertString().length();
