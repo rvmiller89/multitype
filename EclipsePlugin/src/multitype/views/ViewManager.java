56bec4822cea50bdae6526e4f291c03747523b6e
mrcorndog88@gmail.com <mrcorndog88@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-04-08 02:33:05 +0000

    user connected later will receive host information

--- EclipsePlugin/src/multitype/views/ViewManager.java
+++ EclipsePlugin/src/multitype/views/ViewManager.java
@@ -92,10 +92,12 @@ public class ViewManager extends ViewPart{
 			case Request_Host:
 				break;
 			case New_Host:
-				Activator.getDefault().userList.hostId = feu.getUserId();
-				if (Activator.getDefault().userInfo.getUserid() == feu.getUserId())
-					Activator.getDefault().isHost = true;
-				Activator.getDefault().userList.setButton(false);
+				if (feu.getUserId() != -1) {
+					Activator.getDefault().userList.hostId = feu.getUserId();
+					if (Activator.getDefault().userInfo.getUserid() == feu.getUserId())
+						Activator.getDefault().isHost = true;
+					Activator.getDefault().userList.setButton(false);
+				}
 				break;
 			case Console_Message:
 				// Console message received, have ConsoleManager add it to the view
