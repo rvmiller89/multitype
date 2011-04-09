8a71fdb43543d1d01e26fe2b6656292cf218e7a9
mrcorndog88@gmail.com <mrcorndog88@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-04-09 04:51:23 +0000

    Share File Command now shows path of file(limited)

--- EclipsePlugin/src/multitype/command/ShareFile.java
+++ EclipsePlugin/src/multitype/command/ShareFile.java
@@ -3,6 +3,9 @@ package multitype.command;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.internal.resources.File;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -12,8 +15,19 @@ public class ShareFile extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IStructuredSelection selection = (IStructuredSelection) HandlerUtil
 		.getActiveMenuSelection(event);
-		MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
-				"Information", "ShareFile detected on "+ selection.toString());
+		//HandlerUtil.getActiveContexts(event).getClass().toString();
+		if(selection.getFirstElement() instanceof IResource) {
+			IResource file = (IResource) selection.getFirstElement();
+			IPath path = file.getLocation();
+			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
+				"Information", "ShareFile detected on "
+				+ path.toPortableString());
+		}
+		else {
+			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
+					"Information", "ShareFile detected on "
+					+ selection.getFirstElement().getClass().getName());
+		}
 		return null;
 	}
 }
