daf9d0b89ae58ccfb9f54092e2d72bb68448b703
mrcorndog88@gmail.com <mrcorndog88@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-04-09 02:32:40 +0000

    Right Click-> Share File this will now display the location of the selected file

--- EclipsePlugin/src/multitype/command/ShareFile.java
+++ EclipsePlugin/src/multitype/command/ShareFile.java
@@ -3,10 +3,17 @@ package multitype.command;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class ShareFile extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IStructuredSelection selection = (IStructuredSelection) HandlerUtil
+		.getActiveMenuSelection(event);
+		MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
+				"Information", "ShareFile detected on "+ selection.toString());
 		return null;
 	}
 }
