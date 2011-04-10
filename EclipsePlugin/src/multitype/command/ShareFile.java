25618cf7f0aaaec32c1e72b18cc00e586e0addc9
rvmiller89@gmail.com <rvmiller89@gmail.com@a38c68f8-ab39-6fe5-319b-ac8d3413066e>
2011-04-10 06:05:15 +0000

    Finished ShareFile implementation, added ability to select and share multiple files, does folder checking

--- EclipsePlugin/src/multitype/command/ShareFile.java
+++ EclipsePlugin/src/multitype/command/ShareFile.java
@@ -1,33 +1,73 @@
+/**
+ * @author Yeong-ouk Kim and Ryan Miller
+ */
+
 package multitype.command;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import multitype.Activator;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.internal.resources.File;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class ShareFile extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+	    
+
 		IStructuredSelection selection = (IStructuredSelection) HandlerUtil
 		.getActiveMenuSelection(event);
-		//HandlerUtil.getActiveContexts(event).getClass().toString();
-		if(selection.getFirstElement() instanceof IResource) {
-			IResource file = (IResource) selection.getFirstElement();
-			IPath path = file.getLocation();
-			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
-				"Information", "ShareFile detected on "
-				+ path.toPortableString());
-		}
-		else {
-			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
-					"Information", "ShareFile detected on "
-					+ selection.getFirstElement().getClass().getName());
-		}
+		
+		filesToOpen(selection);
+
 		return null;
+	}
+	
+	public void filesToOpen(IStructuredSelection selectionList)
+	{
+		List<String> filePaths = new ArrayList<String>();
+
+		Iterator<Object> iterator = selectionList.iterator();
+		while (iterator.hasNext())
+		{
+			Object o = iterator.next();
+			
+			IResource resource; 
+			
+			if(o instanceof IResource) {
+				resource = (IResource) o;
+			}
+			else {
+		        resource =
+		            (IResource) ((IAdaptable) o)
+		                .getAdapter(IResource.class);
+			}
+			
+			if (resource != null)
+			{
+				
+				if ( resource.getType() == IResource.FILE)
+				{
+					IPath path = resource.getLocation();
+					String filepath = path.toOSString();
+					filePaths.add(filepath);
+				
+					// Debug
+					Activator.getDefault().showDialogAsync("Debug", "Filepath: " + filepath);
+				}
+			}
+		}
+		
+		// TODO
+		// Now call EditorManager foreach filepath in list to open
+		
 	}
 }
